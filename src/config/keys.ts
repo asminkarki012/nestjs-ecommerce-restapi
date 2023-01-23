8c103ba9a80425193965a0f710e2e323d39094c9
asminkarki012 <37731845+asminkarki012@users.noreply.github.com>
2023-01-23 21:49:17 +0545

    Delete keys.ts

--- src/config/keys.ts
+++
@@ -1,9 +0,0 @@
-export default{
-    mongoURI:'mongodb+srv://mongodb:mongodb@cluster0.dkznm.mongodb.net/nestjs-rest-api?retryWrites=true&w=majority',
-    GMAIL_ACC:"photosharing2078@gmail.com",
-    GMAIL_PASS:"sifsngcpravezojw",
-    CLOUD_NAME:"ddtjmyjfi",
-    CLOUD_APIKEY:"994684682835649",
-    CLOUD_APISECRET:"BNLPFllzkCrA8PDdSo03fb1Z5aA"
-
-}
